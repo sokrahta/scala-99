c61c90b364913779df9c8658dbfd548907f02b8b
sokrahta <john.krah@socrata.com>
2014-11-18 14:49:40 -0800

    find out wheter a list is a palindrome

---
+++ p06-isPalindrome.scala
@@ -0,0 +1,11 @@
+def isPalindrome[A](as: List[A]): Boolean = {
+  def go(rs: List[A], as: List[A]): Boolean = (rs, as) match {
+    case (a::Nil, c::Nil) => a==c
+    case (a::b, c::d) => if (a==c) go(b,d) else false
+    case (_,_) => false
+  }
+  go(as.reverse, as)
+}
+
+isPalindrome(List(1,2,3,2,1))
+
