9354aaef7ee50907c539ffcc0dddff8cf9ecf9e3
sokrahta <john.krah@socrata.com>
2014-11-18 15:06:53 -0800

    elimnate consecutive duplicates of list elements

---
+++ p08-compress.scala
@@ -0,0 +1,12 @@
+def compress[A](as: List[A]): List[A] = {
+  def go(rs: List[A], a: A, as: List[A]): List[A] = as match {
+    case h::Nil => if (a==h) rs else h::rs
+    case h::t => if (a==h) go(rs,a,t) else go(h::rs,h,t)
+    case _ => throw new NoSuchElementException
+  }
+  if (as.length <= 1) as
+  else go(as(0)::Nil, as(0), as).reverse
+}
+
+compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+
