a9cfe4f7c74eb737daabbcf582e346e972be60b0
sokrahta <john.krah@socrata.com>
2014-11-19 14:17:21 -0800

    decode a run-length encoded list

---
+++ p12-decode.scala
@@ -0,0 +1,7 @@
+def decode[A](es: List[(Int,A)]): List[A] = {
+  es flatMap { e => List.fill(e._1)(e._2) }
+}
+
+decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+//res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+
