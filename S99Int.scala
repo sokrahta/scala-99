77050b591bda3c8551b7f7daf9b38c6ccb2a0515
sokrahta <john.krah@socrata.com>
2014-12-02 14:34:32 -0800

    p31 determine whether a given integer number is prime

--- S99Int.scala
+++ S99Int.scala
@@ -1,10 +1,19 @@
-package arithmetic {
-  class S99Int(val start: Int) {
-    import S99Int._
-  }
-
-  object S99Int {
-    implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
-  }
+class S99Int(val start: Int) {
+  import S99Int._
+  
+  override def toString: String = start.toString
+  
+  def isPrime: Boolean = (start > 1) && (primes takeWhile {_<=Math.sqrt(start)} forall {start % _ != 0})
+    
 }
+
+object S99Int {
+  implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
+  
+  val primes = Stream.cons(2, Stream.from(3,2) filter {_.isPrime})
+}
+
+import S99Int.int2S99Int
+
+7.isPrime
 
