b29607a9c75e67c7dd4bdcef3995988f5f0ff83c
John Krah <john.krah@socrata.com>
2014-12-04 17:22:18 -0800

    p46 truth tables for logical expressions

--- S99Logic.scala
+++ S99Logic.scala
@@ -6,18 +6,27 @@ object S99Logic {
   def xor (a: Boolean, b: Boolean): Boolean = !equ(a,b)
   def impl(a: Boolean, b: Boolean): Boolean = or(!a,b)
   def equ (a: Boolean, b: Boolean): Boolean = a==b
+
+  def table2(fname: String, f: (Boolean,Boolean) => Boolean): Unit = {
+    def printone(a: String, b: String, r: String) = println("%-5s %-5s %-5s = %s".format(a,fname,b,r))
+    printone("A","B","result")
+    printone("true","true",f(true,true).toString)
+    printone("true","false",f(true,false).toString)
+    printone("false","true",f(false,true).toString)
+    printone("false","false",f(false,false).toString)
+  }
 }
 
 object Exercises {
   import S99Logic._
   def run {
-    println(and(true,true)) //res0: Boolean = true
-    println(or(true,true))
-    println(nand(true,true))
-    println(nor(true,true))
-    println(xor(true,true))
-    println(impl(true,true))
-    println(equ(true,true))
+    table2("and", and)
+    table2("or", or)
+    table2("nand", nand)
+    table2("nor", nor)
+    table2("xor", xor)
+    table2("impl", impl)
+    table2("equ", equ)
   }
 }
 
