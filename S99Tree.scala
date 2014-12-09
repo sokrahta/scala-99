9c12b1a0019b4a3d79809435209f5d3249f80855
John Krah <john.krah@socrata.com>
2014-12-09 10:11:42 -0800

    p55 construct balanced binary trees

--- S99Tree.scala
+++ S99Tree.scala
@@ -1,5 +1,20 @@
 sealed abstract class Tree[+T]
 
+object Tree {
+  def cBalanced[A](n: Int, a: A): List[Tree[A]] = n match {
+    case n if n < 1      => List(End)
+    case n if n % 2 == 1 => {
+      val subtree = cBalanced(n/2, a)
+      subtree.flatMap(l => subtree.map(r => Node(a,l,r) ) )
+    }
+    case n               => {
+      val subtreeL = cBalanced((n-1)/2+1, a)
+      val subtreeR = cBalanced((n-1)/2, a)
+      subtreeL.flatMap(l => subtreeR.flatMap(r => List(Node(a,l,r), Node(a,r,l)) ) )
+    }
+  }
+}
+
 case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
   override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
 }
@@ -11,4 +26,7 @@ case object End extends Tree[Nothing] {
 object Node {
   def apply[T](value: T): Node[T] = Node(value, End, End)
 }
+
+Tree.cBalanced(4, "x")
+//res0: List(Node[String]) = List(T(x T(x . .) T(x . T(x . .))), T(x T(x . .) T(x T(x . .) .)), ...
 
