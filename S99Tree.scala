0010ecfc2ae66d571664e27c83c5988b57b4add9
John Krah <john.krah@socrata.com>
2014-12-09 10:55:59 -0800

    p57 binary search trees

--- S99Tree.scala
+++ S99Tree.scala
@@ -1,6 +1,7 @@
 sealed abstract class Tree[+T] {
   def isSymmetric: Boolean
   def isMirrorOf[S](other: Tree[S]): Boolean
+  def addValue[U >: T <% Ordered[U]](x: U): Tree[U]
 }
 
 object Tree {
@@ -25,12 +26,16 @@ case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
     case x: Node[S] => left.isMirrorOf(x.right) && right.isMirrorOf(x.left)
     case _          => false
   }
+  def addValue[U >: T <% Ordered[U]](x: U): Tree[U] = {
+    if (value > x) Node(value, left.addValue(x), right) else Node(value, left, right.addValue(x))
+  }
 }
 
 case object End extends Tree[Nothing] {
   override def toString = "."
   def isSymmetric: Boolean = true
   def isMirrorOf[S](other: Tree[S]): Boolean = {other == End}
+  def addValue[U <% Ordered[U]](x: U): Tree[U] = Node(x)
 }
 
 object Node {
@@ -41,4 +46,10 @@ Tree.cBalanced(4, "x")
 //res0: List(Node[String]) = List(T(x T(x . .) T(x . T(x . .))), T(x T(x . .) T(x T(x . .) .)), ...
 Node('a', Node('b'), Node('c')).isSymmetric
 //res0: Boolean = true
+val res0 = End.addValue(2)
+//res0: Node[Int] = T(2 . .)
+val res1 = res0.addValue(3)
+//res1: Node[Int] = T(2 . T(3 . .))
+res1.addValue(0)
+//res2: Node[Int] = T(2 T(0 . .) T(3 . .))
 
