a3561d8c2dd6234734a718bfbdb82eff20a15180
John Krah <john.krah@socrata.com>
2014-12-09 10:19:44 -0800

    p56 symmetric binary trees

--- S99Tree.scala
+++ S99Tree.scala
@@ -1,4 +1,7 @@
-sealed abstract class Tree[+T]
+sealed abstract class Tree[+T] {
+  def isSymmetric: Boolean
+  def isMirrorOf[S](other: Tree[S]): Boolean
+}
 
 object Tree {
   def cBalanced[A](n: Int, a: A): List[Tree[A]] = n match {
@@ -17,10 +20,17 @@ object Tree {
 
 case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
   override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+  def isSymmetric: Boolean = left.isMirrorOf(right)
+  def isMirrorOf[S](other: Tree[S]): Boolean = other match {
+    case x: Node[S] => left.isMirrorOf(x.right) && right.isMirrorOf(x.left)
+    case _          => false
+  }
 }
 
 case object End extends Tree[Nothing] {
   override def toString = "."
+  def isSymmetric: Boolean = true
+  def isMirrorOf[S](other: Tree[S]): Boolean = {other == End}
 }
 
 object Node {
@@ -29,4 +39,6 @@ object Node {
 
 Tree.cBalanced(4, "x")
 //res0: List(Node[String]) = List(T(x T(x . .) T(x . T(x . .))), T(x T(x . .) T(x T(x . .) .)), ...
+Node('a', Node('b'), Node('c')).isSymmetric
+//res0: Boolean = true
 
