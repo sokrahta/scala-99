63e3dcf7cc45d375d8495d668f3f862508101078
John Krah <john.krah@socrata.com>
2014-12-12 10:40:32 -0800

    starting definition for new class S99 MTree

---
+++ S99MTree.scala
@@ -0,0 +1,11 @@
+case class MTree[+T](value: T, children: List[MTree[T]]) {
+  def this(value: T) = this(value, List())
+  override def toString = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+}
+
+object MTree {
+  def apply[T](value: T) = new MTree(value, List())
+  def apply[T](value: T, children: List[MTree[T]]) = new MTree(value, children)
+}
+
+val example = MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e')))))
