af4db89e8fdbcd500f3553126115f30b6b2513e6
John Krah <john.krah@socrata.com>
2015-01-01 18:47:19 -0800

    Revert "p90 sudoku strategy 2: hidden singles"
    This reverts commit e5381de4140b80757d910fe944c17fa9e980a2b1.

--- p97-sudoku.scala
+++ p97-sudoku.scala
@@ -1,78 +1,36 @@
 def deserialize(s: String): Array[Array[Char]] =
-  s.trim.replaceAll("[|]","").split("(\r)?\n").zipWithIndex.filter(_._2%2==0).map(_._1.toCharArray.filter(_!=' '))
+  s.replaceAll("[|]","").split("(\r)?\n").zipWithIndex.filter(_._2%2==1).map(_._1.toCharArray.filter(_!=' '))
 
 def serialize(a: Array[Array[Char]]): String =
   a.map(_.sliding(3,3).map(_.mkString("  ")).mkString(" | "))
     .sliding(3,3).map(_.mkString("\n        |         |        \n")).mkString("\n--------+---------+--------\n")
 
-def row[T](p: Array[Array[T]], x: Int): List[T] = p(x).toList
-def col[T](p: Array[Array[T]], y: Int): List[T] = p.map(_(y)).toList
-def box[T](p: Array[Array[T]], x: Int, y: Int): List[T] =
-  p.zipWithIndex.filter(_._2/3==x/3).map(_._1).flatMap(_.zipWithIndex.filter(_._2/3==y/3).map(_._1)).toList
-def box[T](p: Array[Array[T]], n: Int): List[T] = box(p, n%3, n/3)
+def row(p: Array[Array[Char]], x: Int): Array[Char] =
+  p(x)
+
+def col(p: Array[Array[Char]], y: Int): Array[Char] =
+  p.map(_(y))
+
+def box(p: Array[Array[Char]], x: Int, y: Int): Array[Char] =
+  p.zipWithIndex.filter(_._2/3==x/3).map(_._1).flatMap(_.zipWithIndex.filter(_._2/3==y/3).map(_._1))
 
 val series = List('1', '2', '3', '4', '5', '6', '7', '8', '9')
 
-def candidates(p: Array[Array[Char]]): Array[Array[List[Char]]] = {
-  var c = Array.fill(9)(Array.fill(9)(series))
-  for(
-    x <- 0 to 8;
-    y <- 0 to 8)
-  yield {
-    val (v, xb, yb) = (p(x)(y), x/3*3, y/3*3)
-    if (v!='.') {
-      c(x)(y) = List(v)
-      for(x1 <- (0 to 8).filter(_!=x)) yield c(x1)(y) = c(x1)(y).filter(_!=v)
-      for(y1 <- (0 to 8).filter(_!=y)) yield c(x)(y1) = c(x)(y1).filter(_!=v)
-      for(
-        x1 <- (xb to xb+2);
-        y1 <- (yb to yb+2))
-      yield if (x1!=x || y1!=y) c(x1)(y1) = c(x1)(y1).filter(_!=v)
-    }
-  }
-  c
-}
-
 def singles(p: Array[Array[Char]]): (Int, Array[Array[Char]]) = {
-  var (count, cs) = (0, candidates(p))
+  var count: Int = 0
   for(
     x <- 0 to 8;
     y <- 0 to 8)
   yield {
     if (p(x)(y)=='.') {
-      val ws = cs(x)(y)
-      if (ws.length == 1) {
-        count+=1
-        p(x)(y) = ws(0)
+      val (r, c, b) = (row(p,x), col(p,y), box(p,x,y))
+      val w = series.filter(a => !r.contains(a) && !c.contains(a) && !b.contains(a))
+      if (w.isEmpty) {
+        throw new Exception("overconstrained")
       }
-    }
-  }
-  (count,p)
-}
-
-def hiddenSingles(p: Array[Array[Char]]): (Int, Array[Array[Char]]) = {
-  var (count, cs) = (0, candidates(p))
-  for(
-    x <- 0 to 8;
-    y <- 0 to 8)
-  yield {
-    if (p(x)(y)=='.') {
-      val (r, c, b) = (row(cs,x), col(cs,y), box(cs,x,y))
-      val rw = r.flatMap(a=>a).toList.groupBy(a=>a).map(a => (a._1, a._2.length)).filter(_._2==1).map(_._1).toList
-      val cw = c.flatMap(a=>a).toList.groupBy(a=>a).map(a => (a._1, a._2.length)).filter(_._2==1).map(_._1).toList
-      val bw = b.flatMap(a=>a).toList.groupBy(a=>a).map(a => (a._1, a._2.length)).filter(_._2==1).map(_._1).toList
-      val rws = cs(x)(y).filter(a => rw.contains(a))
-      val cws = cs(x)(y).filter(a => cw.contains(a))
-      val bws = cs(x)(y).filter(a => bw.contains(a))
-      if (rws.length == 1) {
+      else if (w.length == 1) {
         count+=1
-        p(x)(y) = rws(0)
-      } else if (cws.length == 1) {
-        count+=1
-        p(x)(y) = cws(0)
-      } else if (bws.length == 1) {
-        count+=1
-        p(x)(y) = bws(0)
+        p(x)(y) = w(0)
       }
     }
   }
@@ -80,21 +38,17 @@ def hiddenSingles(p: Array[Array[Char]]): (Int, Array[Array[Char]]) = {
 }
 
 def solve(problem: String): String = {
-  var iter: Int = 0
   @annotation.tailrec
   def go(acc: Array[Array[Char]]): Array[Array[Char]] = {
-    iter+=1
     if (acc.flatMap(_.find(_=='.')).isEmpty) acc
     else {
       //TODO: add solving rules for more complex sudoku problems
-      val (count1,next1) = singles(acc)
-      val (count2,next2) = hiddenSingles(next1)
-      if (List(count1,count2).sum == 0) acc else go(next2)
+      val (count,next) = singles(acc)
+      if (count == 0) throw new Exception("need more complex solution")
+      go(next)
     }
   }
-  val solved = serialize(go(deserialize(problem)))
-  println(s"solve took %d iterations".format(iter))
-  solved
+  serialize(go(deserialize(problem)))
 }
 
 val sudokuSolved = solve("""
@@ -135,24 +89,4 @@ val testSolution = """
         |         |
 2  4  6 | 3  9  1 | 5  7  8
 """
-
-val sudokuSolved2 = solve("""
-.  .  . | .  .  4 | .  2  8
-        |         |        
-4  .  6 | .  .  . | .  .  5
-        |         |        
-1  .  . | .  3  . | 6  .  .
---------+---------+--------
-.  .  . | 3  .  1 | .  .  .
-        |         |        
-.  8  7 | .  .  . | 1  4  .
-        |         |        
-.  .  . | 7  .  9 | .  .  .
---------+---------+--------
-.  .  2 | .  1  . | .  .  3
-        |         |        
-9  .  . | .  .  . | 5  .  7
-        |         |        
-6  7  . | 4  .  . | .  .  .
-""")
 
