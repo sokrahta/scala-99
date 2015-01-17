b62a302aac1985640b64d82925be45fc0c907e51
John Krah <john.krah@socrata.com>
2015-01-16 17:42:42 -0800

    p93 an arithmetic puzzle - just getting started

---
+++ p93-arithmetic.scala
@@ -0,0 +1,82 @@
+abstract class Expression {
+  def apply: Float
+}
+
+class EqualityExpression(a: Expression, b: Expression) {
+  import EqualityExpression.floatEqual
+  def apply: Boolean = floatEqual(a.apply, b.apply, 0.001F)
+}
+
+case class SingletonExpression(a: Float) extends Expression {
+  def apply = a
+}
+
+case class UnaryExpression(a: Float, op: String) extends Expression {
+  def apply = op match {
+    case "-" => -a
+    case _ => throw new NotImplementedError(s"operator $op not implemented")
+  }
+}
+
+case class BinaryExpression(a: Float, b: Float, op: String) extends Expression {
+  def apply = op match {
+    case "*" => a * b
+    case "/" => a / b
+    case "%" => a % b
+    case "+" => a + b
+    case "-" => a - b
+    case _ => throw new NotImplementedError(s"operator $op not implemented")
+  }
+}
+
+case class TernaryExpression(a: Float, b: Float, c: Float, op: String) extends Expression {
+  def apply = ???
+}
+
+object Expression {
+  def parse(s: String): Expression = {
+    // ORDER OF OPERATIONS MUTHA FUCKA
+    val parensRegex = """(^\(\))*\((.)*(^\(\))*""".r
+    s match {
+      case parensRegex(a, b, c) => ???
+      case _ => ???
+    }
+  }
+}
+
+object EqualityExpression {
+  def parse(s: String): EqualityExpression = {
+    val t = s.split("=")
+    if (t.length != 2) {
+      throw new Exception("looking for just one equals (=) sign please")
+    } else {
+      new EqualityExpression(Expression.parse(t(0)),Expression.parse(t(1)))
+    }
+  }
+
+  def floatEqual(a: Float, b: Float, epsilon: Float): Boolean = {
+    val FLOAT_MIN_NORMAL = 1.17549e-38F
+    val (absA, absB, diff) = (Math.abs(a), Math.abs(b), Math.abs(a - b))
+    
+    if (a == b) {
+      true
+    } else if (a == 0F || b == 0F || diff < FLOAT_MIN_NORMAL) {
+      diff < (epsilon * FLOAT_MIN_NORMAL)
+    } else {
+      diff / (absA + absB) < epsilon
+    }
+  }
+}
+
+def arithmeticIsValid(exp: String): Boolean = {
+  EqualityExpression.parse(exp).apply
+}
+
+def arithmetic(is: List[Int]): List[EqualityExpression] = {
+  Nil
+}
+
+val arith0 = arithmeticIsValid("2-3+5+7 = 11")
+val artih1 = arithmeticIsValid("2 = (3*5+7)/11")
+val answers = arithmetic(List(2,3,5,7,11))
+//2-3+5+7 = 11 or 2 = (3*5+7)/11 (and ten others!).
